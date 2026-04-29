d7cce96bd350d096b1fefc2e810aa8a8a168ea0b
ErickFuentes67 <ejhoanfuentes@unicesar.edu.co>
2026-04-28 21:12:09 -0500

    Feat: atributos BicicletaElectrica

--- src/Model/BicicletaElectrica.java
+++ src/Model/BicicletaElectrica.java
@@ -2,6 +2,21 @@ package Model;
 
 public class BicicletaElectrica extends VehiculoElectrico {
 
+    private int numeroMarchas;
+    private boolean tieneAsistenciaPedal;
+    private String tipoBicicleta;
 
+    public BicicletaElectrica(String id, String marca, String modelo,
+                              Double autonomiaKm, Double capacidadBateria,
+                              Double precioBase, int velocidadMaxima,
+                              int numeroMarchas, boolean tieneAsistenciaPedal,
+                              String tipoBicicleta) {
+
+        super(id, marca, modelo, autonomiaKm, capacidadBateria, precioBase, velocidadMaxima);
+
+        this.numeroMarchas = numeroMarchas;
+        this.tieneAsistenciaPedal = tieneAsistenciaPedal;
+        this.tipoBicicleta = tipoBicicleta;
+    }
 
 }
