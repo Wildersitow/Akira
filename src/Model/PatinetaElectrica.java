5bf4eaf7623d199de2cfc0bdf8f8ff2a4a3dc22e
ErickFuentes67 <ejhoanfuentes@unicesar.edu.co>
2026-04-28 21:11:05 -0500

    Feat: atributos PatinetaElectrica

--- src/Model/PatinetaElectrica.java
+++ src/Model/PatinetaElectrica.java
@@ -2,5 +2,21 @@ package Model;
 
 public class PatinetaElectrica extends VehiculoElectrico {
 
+    private boolean esPlegable;
+    private Double pesoDispositivoKg;
+    private Double pesoMaximoUsuarioKg;
+
+    public PatinetaElectrica(String id, String marca, String modelo,
+                             Double autonomiaKm, Double capacidadBateria,
+                             Double precioBase, int velocidadMaxima,
+                             boolean esPlegable, Double pesoDispositivoKg,
+                             Double pesoMaximoUsuarioKg) {
+
+        super(id, marca, modelo, autonomiaKm, capacidadBateria, precioBase, velocidadMaxima);
+
+        this.esPlegable = esPlegable;
+        this.pesoDispositivoKg = pesoDispositivoKg;
+        this.pesoMaximoUsuarioKg = pesoMaximoUsuarioKg;
+    }
 
 }
