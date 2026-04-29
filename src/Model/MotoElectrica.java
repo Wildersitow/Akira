83364bb69be927045889a8908e86e892f8e672b7
ErickFuentes67 <ejhoanfuentes@unicesar.edu.co>
2026-04-28 21:10:26 -0500

    Feat: atributos MotoElectrica

--- src/Model/MotoElectrica.java
+++ src/Model/MotoElectrica.java
@@ -2,4 +2,20 @@ package Model;
 
 public class MotoElectrica extends VehiculoElectrico {
 
+    private int numeroPasajeros;
+    private String tipoMoto;
+    private String tipoCarga;
+
+    public MotoElectrica(String id, String marca, String modelo,
+                         Double autonomiaKm, Double capacidadBateria,
+                         Double precioBase, int velocidadMaxima,
+                         int numeroPasajeros, String tipoMoto, String tipoCarga) {
+
+        super(id, marca, modelo, autonomiaKm, capacidadBateria, precioBase, velocidadMaxima);
+
+        this.numeroPasajeros = numeroPasajeros;
+        this.tipoMoto = tipoMoto;
+        this.tipoCarga = tipoCarga;
+    }
+
 }
