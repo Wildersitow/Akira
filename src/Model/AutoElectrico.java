7e8445c73c6dfbf28c77e354ab7b14f6722aaa81
ErickFuentes67 <ejhoanfuentes@unicesar.edu.co>
2026-04-28 21:08:54 -0500

    Feat: atributos AutoElectrico

--- src/Model/AutoElectrico.java
+++ src/Model/AutoElectrico.java
@@ -2,6 +2,22 @@ package Model;
 
 public class AutoElectrico extends VehiculoElectrico {
 
+    private int numeroPuertas;
+    private int numeroPasajeros;
+    private String tipoCarga;
+    private String traccion;
 
+    public AutoElectrico(String id, String marca, String modelo,
+                         Double autonomiaKm, Double capacidadBateria,
+                         Double precioBase, int velocidadMaxima,
+                         int numeroPuertas, int numeroPasajeros,
+                         String tipoCarga, String traccion) {
 
+        super(id, marca, modelo, autonomiaKm, capacidadBateria, precioBase, velocidadMaxima);
+
+        this.numeroPuertas = numeroPuertas;
+        this.numeroPasajeros = numeroPasajeros;
+        this.tipoCarga = tipoCarga;
+        this.traccion = traccion;
+    }
 }
--- src/Model/VehiculoElectrico.java
+++ src/Model/VehiculoElectrico.java
@@ -2,8 +2,26 @@ package Model;
 
 public abstract class VehiculoElectrico {
 
+    protected String Id;
     protected String Marca;
-    protected Double Autonomia;
-    protected String TipoVehiculo;
+    protected String Modelo;
+    protected Double AutonomiaKm;
+    protected Double CapacidadBateria;
+    protected Double PrecioBase;
+    protected int VelocidadMaxima;
 
+
+    public VehiculoElectrico(String id, String marca, String modelo,
+                             Double autonomiaKm, Double CapacidadBateria,
+                             Double precioBase, int velocidadMaxima)
+    {
+        this.Id = id;
+        this.Marca = marca;
+        this.Modelo = modelo;
+        this.AutonomiaKm = autonomiaKm;
+        this.CapacidadBateria = CapacidadBateria;
+        this.PrecioBase = precioBase;
+        this.VelocidadMaxima = velocidadMaxima;
+
+    }
 }
