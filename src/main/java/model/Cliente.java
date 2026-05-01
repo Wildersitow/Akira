5867712a1d3b5a2edc10f20f9321a31230570b02
wpverdecia <wpverdecia@unicesar.edu.co>
2026-04-30 22:58:26 -0500

    feat: Asignación de atributos en clase Cliente

--- src/main/java/model/Cliente.java
+++ src/main/java/model/Cliente.java
@@ -2,6 +2,15 @@ package model;
 
 public class Cliente extends Persona {
 
+    private String licenciaConducir;
+    private double historialCredito;
+    private int puntosFidelidad;
+    private List<Contrato> contratos;
 
-
+    public Cliente(String licenciaConducir, double historialCredito, int puntosFidelidad, List<Contrato> contratos) {
+        this.licenciaConducir = licenciaConducir;
+        this.historialCredito = historialCredito;
+        this.puntosFidelidad = puntosFidelidad;
+        this.contratos = contratos;
+    }
 }
