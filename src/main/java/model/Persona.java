d924d5d76520616a759ee8a45087720b774f907f
wpverdecia <wpverdecia@unicesar.edu.co>
2026-04-30 22:01:57 -0500

    feat: Asignación de atributos en clase Persona

--- src/main/java/model/Persona.java
+++ src/main/java/model/Persona.java
@@ -2,6 +2,13 @@ package model;
 
 public abstract  class Persona {
 
+    private int nombre;
+    private int email;
+    private int telefono;
 
-
+    public Persona(int nombre, int email, int telefono) {
+        this.nombre = nombre;
+        this.email = email;
+        this.telefono = telefono;
+    }
 }
