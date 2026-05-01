7acd9423e518baf15012cbe54427da89da944bb3
wpverdecia <wpverdecia@unicesar.edu.co>
2026-04-30 22:37:09 -0500

    feat: Asignación de atributos en clase Empleado

--- src/main/java/model/Empleado.java
+++ src/main/java/model/Empleado.java
@@ -2,6 +2,13 @@ package model;
 
 public class Empleado extends Persona {
 
+    private String codigoEmpleado;
+    private String cargo;
+    private double salario;
 
-
+    public Empleado(String codigoEmpleado, String cargo, double salario) {
+        this.codigoEmpleado = codigoEmpleado;
+        this.cargo = cargo;
+        this.salario = salario;
+    }
 }
