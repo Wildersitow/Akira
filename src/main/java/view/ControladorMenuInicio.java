7fd772938da10ff3131193d35246d5e881f9af32
wpverdecia <wpverdecia@unicesar.edu.co>
2026-05-01 20:19:27 -0500

    feat: Estructura basica de ejecución en menuInicio creada

--- src/main/java/view/ControladorMenuInicio.java
+++ src/main/java/view/ControladorMenuInicio.java
@@ -1,4 +1,20 @@
 package view;
 
-public class ControladorMenuInicio {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class ControladorMenuInicio extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("MenuInicio.fxml"));
+        primaryStage.setTitle("Hola mundo");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) { launch(args); }
 }
