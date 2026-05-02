0f88eed865cf45bf0cc76f11222f3c57eeb1f1f6
wpverdecia <wpverdecia@unicesar.edu.co>
2026-05-01 21:50:53 -0500

    feat: Atributos en persona cambiados a String

--- src/main/java/model/Persona.java
+++ src/main/java/model/Persona.java
@@ -2,11 +2,11 @@ package model;
 
 public abstract  class Persona {
 
-    private int nombre;
-    private int email;
+    private String nombre;
+    private String email;
     private int telefono;
 
-    public Persona(int nombre, int email, int telefono) {
+    public Persona(String nombre, String email, int telefono) {
         this.nombre = nombre;
         this.email = email;
         this.telefono = telefono;
