df5d2b62c396585f86175d76dcc5e3cda6690570
wpverdecia <wpverdecia@unicesar.edu.co>
2026-05-10 03:42:31 -0500

    feat: Atributos en Persona.java actualizados

--- src/main/java/model/Persona.java
+++ src/main/java/model/Persona.java
@@ -3,11 +3,17 @@ package model;
 public abstract  class Persona {
 
     private String nombre;
+    private String nombreUsuario;
+    private String contraseña;
+    private String documentoid;
     private String email;
     private int telefono;
 
-    public Persona(String nombre, String email, int telefono) {
+    public Persona(String nombre, String nombreUsuario, String contraseña, String documentoid, String email, int telefono) {
         this.nombre = nombre;
+        this.nombreUsuario = nombreUsuario;
+        this.contraseña = contraseña;
+        this.documentoid = documentoid;
         this.email = email;
         this.telefono = telefono;
     }
