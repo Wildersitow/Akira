a5c1e6444aa07f7456d62169e966f42eab2f41c6
wpverdecia <wpverdecia@unicesar.edu.co>
2026-05-10 18:12:42 -0500

    feat: Solución confusión con ServiceException y ServiceLogin

--- src/main/java/service/ServiceException.java
+++ src/main/java/service/ServiceException.java
@@ -1,4 +1,21 @@
 package service;
 
-public class ServiceException {
+public class ServiceException extends Exception {
+    private static final long serialVersionUID = 1L;
+
+    private String codigo;
+
+    public ServiceException(String codigo, String mensaje) {
+        super(mensaje);
+        this.codigo = codigo;
+    }
+
+    public ServiceException(String codigo, String mensaje, Throwable causa) {
+        super(mensaje, causa);
+        this.codigo = codigo;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
 }
--- src/main/java/service/ServiceLogin.java
+++ src/main/java/service/ServiceLogin.java
@@ -1,21 +1,5 @@
 package service;
 
-public class ServiceLogin extends Exception {
-    private static final long serialVersionUID = 1L;
+public class ServiceLogin {
 
-    private String codigo;
-
-    public ServiceLogin(String codigo, String mensaje) {
-        super(mensaje);
-        this.codigo = codigo;
-    }
-
-    public ServiceLogin(String codigo, String mensaje, Throwable causa) {
-        super(mensaje, causa);
-        this.codigo = codigo;
-    }
-
-    public String getCodigo() {
-        return codigo;
-    }
 }
