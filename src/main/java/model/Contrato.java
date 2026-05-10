9c622d390c16af6af0a88623d13b55c95ee189cb
wpverdecia <wpverdecia@unicesar.edu.co>
2026-05-10 16:47:59 -0500

    feat: Creación atributos y constructor de contrato

--- src/main/java/model/Contrato.java
+++ src/main/java/model/Contrato.java
@@ -1,5 +1,19 @@
 package model;
 
-public class Contrato {
+import java.util.Date;
 
+public class Contrato {
+    private String producto;
+    private Date fechaContrato;
+    private String persona;
+    private String supervisor;
+    private String precio;
+
+    public Contrato(String producto, Date fechaContrato, String persona, String supervisor, String precio) {
+        this.producto = producto;
+        this.fechaContrato = fechaContrato;
+        this.persona = persona;
+        this.supervisor = supervisor;
+        this.precio = precio;
+    }
 }
