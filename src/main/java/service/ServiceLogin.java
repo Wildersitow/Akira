5be78c660a53baafad28f5f50a594ba41dac8d2f
wpverdecia <wpverdecia@unicesar.edu.co>
2026-05-10 18:10:15 -0500

    feat: Creación logica de excepcion en ServiceLogin

--- src/main/java/service/ServiceLogin.java
+++ src/main/java/service/ServiceLogin.java
@@ -1,4 +1,21 @@
 package service;
 
-public class ServiceLogin {
+public class ServiceLogin extends Exception {
+    private static final long serialVersionUID = 1L;
+
+    private String codigo;
+
+    public ServiceLogin(String codigo, String mensaje) {
+        super(mensaje);
+        this.codigo = codigo;
+    }
+
+    public ServiceLogin(String codigo, String mensaje, Throwable causa) {
+        super(mensaje, causa);
+        this.codigo = codigo;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
 }
