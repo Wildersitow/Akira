31605d68637d0b33878719b4ff8225ee82f7a64c
wpverdecia <wpverdecia@unicesar.edu.co>
2026-05-09 22:04:39 -0500

    feat: Creación de packages DAO y Service

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module org.example.akira {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.kordamp.bootstrapfx.core;
-
-    opens org.example.akira to javafx.fxml;
-    exports org.example.akira;
-}
