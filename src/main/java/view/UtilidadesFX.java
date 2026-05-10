c7c9e35cd36ae0faee2e68cea583cb994943c184
wpverdecia <wpverdecia@unicesar.edu.co>
2026-05-10 17:16:15 -0500

    feat: Creación metodo mostrarAlerta

--- src/main/java/view/UtilidadesFX.java
+++ src/main/java/view/UtilidadesFX.java
@@ -8,6 +8,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
@@ -72,4 +73,13 @@ public class UtilidadesFX {
             e.printStackTrace();
         }
     }
+
+    private void mostrarAlerta(Alert.AlertType tipo, String titulo, String mensaje) {
+        Alert alert = new Alert(tipo);
+        alert.setTitle(titulo);
+        alert.setHeaderText(null);
+        alert.setContentText(mensaje);
+        alert.showAndWait();
+    }
+
 }
