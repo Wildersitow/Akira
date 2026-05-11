15cc6339aca4cf1284528322843f6f699cecea73
wpverdecia <wpverdecia@unicesar.edu.co>
2026-05-11 16:56:57 -0500

    feat: Creación metodo cambiarIniciarSesión en ControladorRegister

--- src/main/java/view/ControladorRegister.java
+++ src/main/java/view/ControladorRegister.java
@@ -149,4 +149,15 @@ public class ControladorRegister {
         }
     }
 
+    @FXML
+    public void cambiarIniciarSesion(ActionEvent event) {
+        try {
+            System.out.println("Navegando a Iniciar Sesión...");
+            utilidades.cambiarEscenaConTransicion(event, "/src/main/resources/FXML/Login.fxml");
+        } catch (Exception e) {
+            System.err.println("ERROR al cambiar escena:");
+            e.printStackTrace();
+        }
+    }
+
 }
