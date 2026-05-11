c013bcf33887c673e0af8b72c55049fdcb53d6b8
wpverdecia <wpverdecia@unicesar.edu.co>
2026-05-11 15:26:39 -0500

    feat: Creación metodo guardar

--- src/main/java/dao/EmpleadoDAO.java
+++ src/main/java/dao/EmpleadoDAO.java
@@ -1,4 +1,34 @@
 package dao;
 
+import model.Empleado;
+import service.ServiceException;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class EmpleadoDAO {
+
+    private static final String ARCHIVO = "empleado.dat";
+
+    public void guardar(Empleado empleado) throws ServiceException {
+        try {
+            ArrayList<Empleado> empleados = leer();
+
+            // Verificar si el nombre de usuario ya existe
+            for (Empleado e : empleados) {
+                if (e.getNombreUsuario().equalsIgnoreCase(empleado.getNombreUsuario())) {
+                    throw new ServiceException("USUARIO_DUPLICADO",
+                            "El nombre de usuario '" + empleado.getNombreUsuario() + "' ya está en uso");
+                }
+            }
+
+            empleados.add(empleado);
+            guardarLista(empleados);
+
+        } catch (IOException e) {
+            throw new ServiceException("ERROR_GUARDADO",
+                    "Error al guardar empleado: " + e.getMessage(), e);
+        }
+    }
+
 }
