2b4dada33682521d89d982e5d99f415c95611e28
ErickFuentes67 <ejhoanfuentes@unicesar.edu.co>
2026-05-12 16:40:01 -0500

    Feat: metodos en AutoElectrico

--- src/main/java/model/AutoElectrico.java
+++ src/main/java/model/AutoElectrico.java
@@ -4,20 +4,46 @@ public class AutoElectrico extends VehiculoElectrico {
 
     private final int numeroPuertas;
     private final int numeroPasajeros;
-    private final String tipoCarga;
+    private final String tipoCarro;
     private final String traccion;
 
-    public AutoElectrico(String id, String marca, String modelo,
-                         Double autonomiaKm, Double capacidadBateria,
-                         Double precioBase, int velocidadMaxima,
-                         int numeroPuertas, int numeroPasajeros,
-                         String tipoCarga, String traccion) {
-
-        super(id, marca, modelo, autonomiaKm, capacidadBateria, precioBase, velocidadMaxima);
-
-        this.numeroPuertas = numeroPuertas;
+    public AutoElectrico(int anio, Double autonomiaKm, Double capacidadBateria, String color, EstadoVehiculo estado, String id, String marca, String modelo, Double precioBase, int potenciaMotorKW, int velocidadMaxima, int numeroPasajeros, int numeroPuertas, String tipoCarro, String traccion) {
+        super(anio, autonomiaKm, capacidadBateria, color, estado, id, marca, modelo, precioBase, potenciaMotorKW, velocidadMaxima);
         this.numeroPasajeros = numeroPasajeros;
-        this.tipoCarga = tipoCarga;
+        this.numeroPuertas = numeroPuertas;
+        this.tipoCarro = tipoCarro;
         this.traccion = traccion;
+    }
+
+    @Override
+    public double calcularPrecioFinal(){
+
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return "AutoEléctrico | " + super.toString() +
+                " | Tipo: " + tipoCarro +
+                " | Puertas: " + numeroPuertas +
+                " | Pasajeros: " + numeroPasajeros +
+                " | Tracción: " + traccion;
+    }
+
+
+    public int getNumeroPasajeros() {
+        return numeroPasajeros;
+    }
+
+    public int getNumeroPuertas() {
+        return numeroPuertas;
+    }
+
+    public String getTipoCarro() {
+        return tipoCarro;
+    }
+
+    public String getTraccion() {
+        return traccion;
     }
 }
