5a6eb35fbc7ed7b4138fe2813934a43de25bc1ac
ErickFuentes67 <ejhoanfuentes@unicesar.edu.co>
2026-05-12 16:52:51 -0500

    Feat: metodos en PatinetaElectrica.java

--- src/main/java/model/PatinetaElectrica.java
+++ src/main/java/model/PatinetaElectrica.java
@@ -2,21 +2,51 @@ package model;
 
 public class PatinetaElectrica extends VehiculoElectrico {
 
-    private final boolean esPlegable;
-    private final Double pesoDispositivoKg;
-    private final Double pesoMaximoUsuarioKg;
+    private int velocidadMaximaKmH;
+    private int cargaMaximaKg;
+    private boolean esPlegable;
 
-    public PatinetaElectrica(String id, String marca, String modelo,
-                             Double autonomiaKm, Double capacidadBateria,
-                             Double precioBase, int velocidadMaxima,
-                             boolean esPlegable, Double pesoDispositivoKg,
-                             Double pesoMaximoUsuarioKg) {
-
-        super(id, marca, modelo, autonomiaKm, capacidadBateria, precioBase, velocidadMaxima);
-
+    public PatinetaElectrica(int anio, Double autonomiaKm, Double capacidadBateria, String color, EstadoVehiculo estado, String id, String marca, String modelo, Double precioBase, int potenciaMotorKW, int velocidadMaxima, int cargaMaximaKg, boolean esPlegable, int velocidadMaximaKmH) {
+        super(anio, autonomiaKm, capacidadBateria, color, estado, id, marca, modelo, precioBase, potenciaMotorKW, velocidadMaxima);
+        this.cargaMaximaKg = cargaMaximaKg;
         this.esPlegable = esPlegable;
-        this.pesoDispositivoKg = pesoDispositivoKg;
-        this.pesoMaximoUsuarioKg = pesoMaximoUsuarioKg;
+        this.velocidadMaximaKmH = velocidadMaximaKmH;
     }
 
+    @Override
+    public double calcularPrecioFinal() {
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return "PatinetaEléctrica | " + super.toString() +
+                " | Vel. máx: " + velocidadMaximaKmH + " km/h" +
+                " | Plegable: " + (esPlegable? "Sí" : "No") +
+                " | Carga máx: " + cargaMaximaKg + " kg";
+    }
+
+    public int getCargaMaximaKg() {
+        return cargaMaximaKg;
+    }
+
+    public void setCargaMaximaKg(int cargaMaximaKg) {
+        this.cargaMaximaKg = cargaMaximaKg;
+    }
+
+    public boolean isEsPlegable() {
+        return esPlegable;
+    }
+
+    public void setEsPlegable(boolean esPlegable) {
+        this.esPlegable = esPlegable;
+    }
+
+    public int getVelocidadMaximaKmH() {
+        return velocidadMaximaKmH;
+    }
+
+    public void setVelocidadMaximaKmH(int velocidadMaximaKmH) {
+        this.velocidadMaximaKmH = velocidadMaximaKmH;
+    }
 }
