ca60f6a3dadc24c6ccd998e427bc1ea501d58ebb
ErickFuentes67 <ejhoanfuentes@unicesar.edu.co>
2026-05-29 10:24:58 -0500

    Feat: atributos en AkiraAssistantService.java

--- src/main/java/service/AkiraAssistantService.java
+++ src/main/java/service/AkiraAssistantService.java
@@ -17,4 +17,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class AkiraAssistantService {
+
+    private static final String API_URL = "https://api.anthropic.com/v1/messages";
+    private static final String API_KEY = "TU_API_KEY_AQUI";
+    private static final String MODEL   = "claude-sonnet-4-20250514";
+
+    private final List<String[]> historial = new ArrayList<>();
+
+    private final AutoElectricoDAO      autoDAO = new AutoElectricoDAO();
+    private final MotoElectricaDAO      motoDAO = new MotoElectricaDAO();
+    private final BicicletaElectricaDAO biciDAO = new BicicletaElectricaDAO();
+    private final PatinetaElectricaDAO  patiDAO = new PatinetaElectricaDAO();
+
 }
